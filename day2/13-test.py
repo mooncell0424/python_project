3a4a81ac21d794e7bfc981f1fe902cf4644ed94f
mooncell0424 <mooncell0424@gmail.com>
2026-01-27 15:58:59 +0800

    test

--- day2/13-test.py
+++ day2/13-test.py
@@ -1,4 +1,9 @@
 # 作者:moon cell
 # 2026年01月21日20时20分29秒
 # 2912480050@qq.com
-print('我要使用git')
+# print('我要使用git')
+# 最简单的例子
+my_dict = {'name': 'Alice', 'age': 25}
+print(list(my_dict.items()))
+for i in my_dict.items():
+    print(i)
