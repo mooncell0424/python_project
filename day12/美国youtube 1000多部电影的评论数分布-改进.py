7de336f05240c356527f9489522afd145bef3465
mooncell0424 <mooncell0424@gmail.com>
2026-02-01 17:46:10 +0800

    test

---
+++ day12/美国youtube 1000多部电影的评论数分布-改进.py
@@ -0,0 +1,19 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+us_file_path = "./youtube_video_data/US_video_data_numbers.csv"
+uk_file_path = "./youtube_video_data/GB_video_data_numbers.csv"
+
+# t1 = np.loadtxt(us_file_path,delimiter=",",dtype="int",unpack=True)
+t_us = np.loadtxt(us_file_path, delimiter=",", dtype="int")
+
+# 取美国的电影的评论的数据
+t_us_comments = t_us[:, -1]
+
+# 选择比5000小的数据
+t_us_comments = t_us_comments[t_us_comments <= 5000]
+
+print(t_us_comments.shape)
+
+# 再看最小值和最大值
+print(t_us_comments.max(), t_us_comments.min())
